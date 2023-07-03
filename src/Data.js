95f1d36c5b18a8752b6c1de74aa340782a5da4d8
Sinan-07 <110185111+Sinan-07@users.noreply.github.com>
2023-07-03 19:36:09 +0530

    first commit
    just uploaded the files in my repository

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "breakfast food",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-5.jpeg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "lunch food",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-6.jpeg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "chats food",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-7.jpeg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "dinner food",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-8.jpeg",
+    },
+  ];
